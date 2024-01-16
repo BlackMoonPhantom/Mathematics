5d9a9447aa420d1a02b979299b98b4ab25f93659
BlackMoonPhantom <nathan.tandon@gmail.com>
2024-01-16 09:27:21 -0500

    math

---
+++ Mathematics/AdvMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    public class AdvMath
+    {   public double Area(double height, double width)
+        {
+            return height * width;
+        }
+        public double Average(params double[] numbers)
+        {
+            double sum = 0;
+            foreach (double num in numbers)
+            {
+                sum += num;
+            }
+
+            return sum / numbers.Length;
+        }
+        public double Squared(double value)
+        {
+            return value * value;
+        }
+        public double PythagoreanTheorem(double a, double b)
+        {
+            double cSquared = Squared(a) + Squared(b);
+            return Math.Sqrt(cSquared);
+        }
+
+    }
+
+}
---
+++ MathematicsTest/AdvMathTest.cs
@@ -0,0 +1,41 @@
+﻿using Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTest
+{
+    public class AdvMathTest
+    {
+        [Fact]
+        public void TestArea()
+        {
+            AdvMath advMath = new AdvMath();
+            double result = advMath.Area(5, 8);
+            Assert.True(result == 40);
+        }
+        [Fact]
+        public void TestAverage()
+        {
+            AdvMath advMath = new AdvMath();
+            double result = advMath.Average(2, 4, 6, 8);
+            Assert.True(result == 5);
+        }
+        [Fact]
+        public void TestSquared()
+        {
+            AdvMath advMath = new AdvMath();
+            double result = advMath.Squared(3);
+            Assert.True(result == 9);
+        }
+        [Fact]
+        public void TestPythagoreanTheorem()
+        {
+            AdvMath advMath = new AdvMath();
+            double result = advMath.PythagoreanTheorem(3, 4);
+            Assert.True(result == 5);
+        }
+    }
+}
